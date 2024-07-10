69986eb733f304e18bb0cc97b598751332c8acb4
Curio5813 <marcosglasner@gmail.com>
2024-07-10 16:06:00 +0100

    Adding SQL File japan_population.sql to the repository."

--- revising_aggregations_averages.sql
+++ revising_aggregations_averages.sql
@@ -1,5 +1,5 @@
 /*
-Query the average population of all cities in CITY where District is California.
+Query the average population for all cities in CITY, rounded down to the nearest integer.
  */
 
 CREATE TABLE CITY (
@@ -10,4 +10,4 @@ CREATE TABLE CITY (
     POPULATION NUMERIC NOT NULL
 );
 
-SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+SELECT ROUND(AVG(POPULATION), 0) FROM CITY;

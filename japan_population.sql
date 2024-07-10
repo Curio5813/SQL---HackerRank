8ec29c97f8f5c901a0f5fa4a7963efd606fb1541
Curio5813 <marcosglasner@gmail.com>
2024-07-10 16:22:29 +0100

    Adding SQL File population_density_difference .sql to the repository."

--- japan_population.sql
+++ japan_population.sql
@@ -2,5 +2,5 @@
  Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
 */
 
-SELECT SUM(CITY.POPULATION) FROM CITY WHERE COUNTRYCODE = 'JPN';
+SELECT
 

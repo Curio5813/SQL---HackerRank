ea0bba6709b83572007543683a45b8a156d64730
Curio5813 <marcosglasner@gmail.com>
2024-07-16 17:11:45 +0100

    Updating SQL file weather_observation_station_17.sql

--- weather_observation_station_17.sql
+++ weather_observation_station_17.sql
@@ -1,7 +1,7 @@
 /*
-Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N)
-in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater
+than 38.7780. Round your answer to 4 decimal places.
 */
 
 
-SELECT ROUND(MIN(STATION.LAT_N), 4) FROM STATION WHERE STATION.LAT_N > 38.7780;
+SELECT ROUND(STATION.LONG_W, 4) FROM STATION WHERE STATION.LAT_N > 38.7780 ORDER BY STATION.LAT_N ASC LIMIT 1;
